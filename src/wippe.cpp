654795feca33f7830cc575f60bd1c7aa7ce1fb37
Saecki <tobiasschmitz2001@gmail.com>
2020-08-12 15:50:24 +0100

    update

--- src/wippe.cpp
+++ src/wippe.cpp
@@ -1,17 +1,6 @@
 #include <chrono>
-#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <pthread.h>
-#include <raspicam/raspicam.h>
 #include <raspicam/raspicam_cv.h>
-
-static volatile bool running = true;
-
-static void* userInput(void*) {
-    while (running) {
-	if (std::cin.get() == 'q') running = false;
-    }
-}
 
 uint64_t mstime() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -20,15 +9,13 @@ uint64_t mstime() {
 }
 
 int main(int argc, char **argv) {
-    std::cout<<"Starting\n";
-    pthread_t threadId;
-    (void) pthread_create(&threadId, 0, userInput, 0);
-
-    std::cout<<"Press q Enter to exit\n";
+    std::cout<<"Starting\nPress q to exit\n";
 
     uint64_t start, end;
     raspicam::RaspiCam_Cv camera;
-    cv::Mat image;
+    cv::Mat imageOriginal;
+    cv::Mat imageHsv;
+    cv::Mat imageThresh;
 
     camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
 
@@ -40,25 +27,29 @@ int main(int argc, char **argv) {
 
     std::cout<<"Capturing frames\n";
 
-    int i = 0;
-    while (running) {
+    uint8_t i = 0;
+    char inputKey = 0;
+    while (inputKey != 27) {
 	if (i % 5 == 0) start = mstime();
         camera.grab();
-        camera.retrieve(image);
+        camera.retrieve(imageOriginal);
         
 	if (i % 5 == 0) {
 	    end = mstime();
 	    uint64_t msdiff = end - start;
-	    std::cout<<"\rFrametime: "<<msdiff<<"ms  "<<std::flush; 
+	    std::cout<<"\rCapturetime: "<<msdiff<<"ms  "<<std::flush; 
 	    i = 0;
 	}
+
+        imageHsv = 
+
 	++i;
+        inputKey = cv::waitKey(1);
     }
 
     std::cout<<"\nStopping camera\n";
     camera.release();
 
-    (void) pthread_join(threadId, NULL);
     std::cout<<"Done\n";
 
     return 0;
