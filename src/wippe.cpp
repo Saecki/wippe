cbe8ab62ff205e463a1eaa0548192e6ceb25a50d
Saecki <tobiasschmitz2001@gmail.com>
2020-08-19 16:13:54 +0100

    upate

--- src/wippe.cpp
+++ src/wippe.cpp
@@ -5,22 +5,15 @@
 #include <raspicam/raspicam_cv.h>
 #include <wiringSerial.h>
 
-#define RGB_UPPER_MARGIN 30
-#define RGB_LOWER_MARGIN 30
+#define RGB_UPPER_MARGIN 30 
+#define RGB_LOWER_MARGIN 30 
 
 enum class State {
     Detecting,
     Tracking
 };
 
-enum class Signal {
-    None,
-    Restart
-};
-
 static volatile bool running = true;
-static volatile bool printColor = false;
-static volatile Signal signal = Signal::None;
 static uint8_t lowB, highB, lowG, highG, lowR, highR;
 
 static void* userInput(void*) {
@@ -29,12 +22,7 @@ static void* userInput(void*) {
         std::cin>>in;
 	if (in == 'q') {
             running = false; 
-        } else if (in == 'r') {
-            signal = Signal::Restart;
-            while (signal == Signal::Restart) continue;
-        } else if (in == 'c') {
-            printColor = !printColor;
-        }
+        } 
     } 
 }
 
@@ -42,42 +30,6 @@ uint64_t mstime() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(
 	std::chrono::system_clock::now().time_since_epoch()
     ).count();
-}
-
-void captureColors(raspicam::RaspiCam_Cv& camera) {
-    cv::Mat imgOriginal;
-    cv::Mat imgThresh;
-    char in;
-    do {
-        camera.grab();
-        camera.retrieve(imgOriginal);
-
-        int width = imgOriginal.cols;
-        int height = imgOriginal.rows;
-        int radius = std::min(width, height) / 40;
-
-        cv::Mat mask = cv::Mat::zeros(imgOriginal.size(), CV_8UC1);
-        cv::circle(mask, cv::Point(width / 2, height / 2), radius, 1);
-
-        cv::Scalar mean = cv::mean(imgOriginal, mask);
-
-        uint8_t b = mean[0];
-        uint8_t g = mean[1];
-        uint8_t r = mean[2];
-
-        printf("b: %u, g: %u, r: %u\n", b, g, r);
-
-        lowB = std::clamp(b - RGB_LOWER_MARGIN, 0, 255);
-        highB = std::clamp(b + RGB_UPPER_MARGIN, 0, 255);
-        lowG = std::clamp(g - RGB_LOWER_MARGIN, 0, 255);
-        highG = std::clamp(g + RGB_UPPER_MARGIN, 0, 255);
-        lowR = std::clamp(r - RGB_LOWER_MARGIN, 0, 255);
-        highR = std::clamp(r + RGB_UPPER_MARGIN, 0, 255);
-
-        std::cout<<"Press y enter to continue or any other character to retry:"<<std::flush;
-        std::cin>>in;
-    }
-    while (in != 'y');
 }
 
 int main(int argc, char **argv) {
@@ -104,9 +56,8 @@ int main(int argc, char **argv) {
 	return 1;
     }
 
-    std::cout<<"Press enter to capture a closeup piture of the obejct you want to track"<<std::flush;
+    std::cout<<"Press enter to continue"<<std::flush;
     while (std::cin.get() != '\n');
-    captureColors(camera);
 
     (void) pthread_create(&threadId, 0, userInput, 0);
     
@@ -126,18 +77,12 @@ int main(int argc, char **argv) {
     uint64_t t2 = 0;
     uint64_t t3 = 0;
     while (running) {
-        if (signal == Signal::Restart) {
-            captureColors(camera);
-            signal = Signal::None;
-            state = State::Detecting;
-        }
 
         if (state == State::Detecting) {
             printf("DETECTING ");
         } else if (state == State::Tracking) {
             printf("TRACKING ");
         }
-
 
 	t1 = mstime();
 
@@ -146,9 +91,19 @@ int main(int argc, char **argv) {
         t2 = mstime();
 
         if (state == State::Detecting) {
-            cv::inRange(imgOriginal, cv::Scalar(lowB, lowG, lowR), cv::Scalar(highB, highG, highR), imgThresh);
+            cv::cvtColor(imgOriginal, imgThresh, cv::COLOR_BGR2GRAY);
             cv::GaussianBlur(imgThresh, imgThresh, cv::Size(3, 3), 0);
-            cv::HoughCircles(imgThresh, circles, CV_HOUGH_GRADIENT, 2, 1000, 100, 100, imgOriginal.cols / 25, imgOriginal.cols / 5);
+            cv::HoughCircles(
+                imgThresh,
+                circles,
+                CV_HOUGH_GRADIENT,
+                2,
+                imgOriginal.cols * 2,
+                100,
+                100,
+                imgOriginal.cols / 25,
+                imgOriginal.cols / 5
+            );
 
             if (circles.size() != 0) {
                 int closestDiff = std::abs(circles[0][0] - xpos) + std::abs(circles[0][1] - ypos);
@@ -193,30 +148,18 @@ int main(int argc, char **argv) {
         
         serialPrintf(serialFD, "%f,%f\n", xpos, ypos);
         
-        if (printColor) {
-            int width = imgOriginal.cols;
-            int height = imgOriginal.rows;
-            int radius = std::min(width, height) / 40;
-            cv::Mat mask = cv::Mat::zeros(imgOriginal.size(), CV_8UC1);
-            cv::circle(mask, cv::Point(width / 2, height / 2), radius, 1);
-            cv::Scalar mean = cv::mean(imgOriginal, mask);
-            uint8_t b = mean[0];
-            uint8_t g = mean[1];
-            uint8_t r = mean[2];
-            printf("b: %u, g: %u, r: %u\n", b, g, r);
-        } else {
-            uint64_t capture = t2 - t1;
-            uint64_t calc = t3 - t2;
-            printf("x: %f,y: %f", xpos, ypos);
-            std::cout<<", capture: "<<capture<<"ms";
-            std::cout<<", calc: "<<calc<<"ms\n";
-        }
+        uint64_t capture = t2 - t1;
+        uint64_t calc = t3 - t2;
+        printf("x: %f,y: %f", xpos, ypos);
+        std::cout<<", capture: "<<capture<<"ms";
+        std::cout<<", calc: "<<calc<<"ms\n";
     }
 
     printf("Stopping camera\n");
     camera.release();
 
     printf("Closing serial port\n");
+    serialPrintf(serialFD, "0.5,0.5\n");
     serialPrintf(serialFD, "0.5,0.5\n");
     serialPrintf(serialFD, "0.5,0.5\n");
     serialClose(serialFD);
