aed6e23003ed1ca9be03ebd5735c44825d39b673
Saecki <tobiasschmitz2001@gmail.com>
2020-10-25 20:36:16 +0000

    update

--- src/wippe.cpp
+++ src/wippe.cpp
@@ -24,14 +24,27 @@ uint64_t mstime() {
     ).count();
 }
 
+#define FACTOR 100.0
+double flatten(double a) {
+    if (a == 0.0) return a;
+    if (a > 0.01) return a;
+    
+    double v = std::abs(a);
+    double f = std::pow(v * FACTOR, 1.8) / FACTOR;
+
+    if (f == 0.0) return f;
+
+    return std::clamp(a * (f / v), -0.5, 0.5);
+}
+
 enum class State {
     Detecting,
     Tracking
 };
 
-struct Position {
-    float x;
-    float y;
+struct Point {
+    double x;
+    double y;
 };
 
 class PositionTracker {
@@ -45,7 +58,7 @@ class PositionTracker {
         cv::Mat imgThresh;
         
         cv::Ptr<cv::Tracker> tracker;
-        cv::Rect2d box;
+        cv::Rect2d roi;
 
     public:
         PositionTracker() {
@@ -68,24 +81,24 @@ class PositionTracker {
             camera.retrieve(imgOriginal);
         }
 
-        std::optional<Position> track() {
-            Position pos;
+        std::optional<Point> track() {
+            Point pos;
 
             if (state == State::Detecting) {
                 std::vector<cv::Vec3f> circles;
 
                 cv::cvtColor(imgOriginal, imgThresh, cv::COLOR_BGR2GRAY);
                 cv::GaussianBlur(imgThresh, imgThresh, cv::Size(3, 3), 0);
-                //cv::dilate(imgThresh, imgThresh, 5);
-                //cv::erode(imgThresh, imgThresh, 5);
+                cv::dilate(imgThresh, imgThresh, 5);
+                cv::erode(imgThresh, imgThresh, 5);
                 cv::HoughCircles(
                     imgThresh,
                     circles,
                     CV_HOUGH_GRADIENT,      // method
                     2,                      // inverse ratio of accumulator resolution
                     imgOriginal.cols * 2,   // minDist
-                    100,                    // param1 depends on method
-                    100,                    // param2 depends on method
+                    100,                    // param1: behavior depends on method
+                    100,                    // param2: behavior depends on method
                     imgOriginal.cols / 25,  // minRadius
                     imgOriginal.cols / 5    // maxRadius
                 );
@@ -101,17 +114,17 @@ class PositionTracker {
                         }
                     }
 
-                    float factor = (float) imgThresh.rows / (float) imgThresh.cols;
-                    float xraw = (float) circles[closest][0];
-                    float yraw = (float) circles[closest][1];
+                    double factor = (double) imgThresh.rows / (double) imgThresh.cols;
+                    double xraw = (double) circles[closest][0];
+                    double yraw = (double) circles[closest][1];
                     
-                    pos.x = xraw / (float) imgThresh.cols;
-                    pos.y = yraw / (float) imgThresh.rows * (factor + (1 - factor) / 2);
-                    float radius = (float) circles[closest][2];
+                    pos.x = xraw / (double) imgThresh.cols;
+                    pos.y = yraw / (double) imgThresh.rows * (factor + (1 - factor) / 2);
+                    double radius = (double) circles[closest][2];
 
                     tracker = cv::TrackerKCF::create();
-                    box = cv::Rect2d(xraw - radius, yraw - radius, radius * 2, radius * 2);
-                    bool ok = tracker->init(imgOriginal, box);
+                    roi = cv::Rect2d(xraw - radius, yraw - radius, radius * 2, radius * 2);
+                    bool ok = tracker->init(imgOriginal, roi);
                     
                     if (ok) {
                         state = State::Tracking;
@@ -122,12 +135,12 @@ class PositionTracker {
                     return {};
                 }
             } else if (state == State::Tracking) {
-                bool ok = tracker->update(imgOriginal, box);
+                bool ok = tracker->update(imgOriginal, roi);
 
                 if (ok) {
-                    float factor = (float) imgThresh.rows / (float) imgThresh.cols;
-                    pos.x = (box.x + box.width / 2) / (float) imgThresh.cols;
-                    pos.y = (box.y + box.height / 2) / (float) imgThresh.rows * (factor + (1 - factor) / 2);
+                    double factor = (double) imgThresh.rows / (double) imgThresh.cols;
+                    pos.x = (roi.x + (double) roi.width / 2.0) / (double) imgThresh.cols;
+                    pos.y = ((roi.y + (double) roi.height / 2.0) / (double) imgThresh.rows) * (factor + (1 - factor) / 2);
                 } else {
                     state = State::Detecting;
                 }
@@ -138,38 +151,58 @@ class PositionTracker {
 };
 
 class PIDController {
-    float PF = 0.7;
-    float IF = 0;
-    float DF = 180;
+    #define HIST_SIZE 2
+    double dxHist[HIST_SIZE];
+    double dyHist[HIST_SIZE];
+
+    double PF = 0.4;
+    double IF = 0;
+    double DF = 220;
     
     uint64_t lastUpdateTime = 0;
-    float lastDiffX = 0;
-    float lastDiffY = 0;
-    float ix = 0;
-    float iy = 0;
+    Point lastDiff;
+    Point i;
 
     public:
-        Position calc(float x, float y) {
+        void reinit(double lastX, double lastY) {
+            lastDiff = { 0.5 - lastY, 0.5 - lastY };
+        }
+
+        Point calc(double x, double y) {
             uint64_t currentTime = mstime();
             
-            float diffX = 0.5 - x;
-            float diffY = 0.5 - y;
-            float timeDiff = currentTime - lastUpdateTime;
+            Point diff = { 0.5 - x, 0.5 - y };
+            double timeDiff = currentTime - lastUpdateTime;
             
-            float px = diffX * PF;
-            float py = diffY * PF;
+            float px = diff.x * PF;
+            float py = diff.y * PF;
 
-            float dx = (diffX - lastDiffX) * DF / timeDiff;
-            float dy = (diffY - lastDiffY) * DF / timeDiff;
+            i.x += diff.x * IF / timeDiff;
+            i.y += diff.y * IF / timeDiff;
             
-            ix += diffX * IF / timeDiff;
-            iy += diffY * IF / timeDiff;
+            float ndx = flatten(diff.x - lastDiff.x) * DF / timeDiff;
+            float ndy = flatten(diff.y - lastDiff.y) * DF / timeDiff;
 
-            float calcX = std::clamp(0.5f + px + ix + dx, 0.0f, 1.0f);
-            float calcY = std::clamp(0.5f + py + iy + dy, 0.0f, 1.0f);
+            for (int i = 0; i < HIST_SIZE - 1; i++) {
+                dxHist[i] = dxHist[i + 1];
+                dyHist[i] = dyHist[i + 1];
+            }
+            dxHist[HIST_SIZE - 1] = ndx;
+            dyHist[HIST_SIZE - 1] = ndy;
+            
+            double dxsum = 0.0;
+            double dysum = 0.0;
+            for (int i = 0; i < HIST_SIZE; i++) {
+                dxsum += dxHist[i];
+                dysum += dyHist[i];
+            }
+            double dx = dxsum / (double)HIST_SIZE; 
+            double dy = dysum / (double)HIST_SIZE;
 
-            lastDiffX = diffX;
-            lastDiffY = diffY;
+            double calcX = std::clamp(0.5 + px + i.x + dx, 0.0, 1.0);
+            double calcY = std::clamp(0.5 + py + i.y + dy, 0.0, 1.0);
+
+            lastDiff = diff;
             lastUpdateTime = currentTime;
 
             return { calcX, calcY };
@@ -178,14 +211,12 @@ class PIDController {
 
 int main(int argc, char **argv) {
     pthread_t threadId;
-    uint64_t t1 = 0;
-    uint64_t t2 = 0;
-    uint64_t t3 = 0;
 
     printf("Starting\n");
     
     PositionTracker tracker;
     PIDController controller;
+    bool tracking = false;
     
     printf("Opening serial port\n");
     int serialFD = serialOpen("/dev/ttyUSB0", 9600);
@@ -200,36 +231,47 @@ int main(int argc, char **argv) {
     printf("Press q enter to exit\n");
 
     while (running) {
-	t1 = mstime();
+	uint64_t t1 = mstime();
         
         tracker.capture();
         
-        t2 = mstime();
+        uint64_t t2 = mstime();
        
-        Position bpos;
+        Point bpos;
         if (auto pos = tracker.track(); pos) {
             bpos = pos.value();
+            
+            if (!tracking) {
+                controller.reinit(bpos.x, bpos.y);
+                tracking = true;
+            }
         } else {
             serialPrintf(serialFD, "0.5,0.5\n");
             printf("Nothing detected\n");
+            tracking = false;
             continue;
         }
+        
+        uint64_t t3 = mstime();
+
+        Point cpos = controller.calc(bpos.x, bpos.y);
+
+        uint64_t t4 = mstime();
+        
+        serialPrintf(serialFD,"%f,%f\n", cpos.x, cpos.y);
+        
+        uint64_t capture = t2 - t1;
+        uint64_t track = t3 - t2;
+        uint64_t calc = t4 - t3;
+        
         if (tracker.state == State::Detecting) {
             printf("DETECTING ");
         } else if (tracker.state == State::Tracking) {
             printf("TRACKING ");
         }
-        
-        t3 = mstime();
-
-        Position cpos = controller.calc(bpos.x, bpos.y);
-        
-        serialPrintf(serialFD,"%f,%f\n", cpos.x, cpos.y);
-        
-        uint64_t capture = t2 - t1;
-        uint64_t calc = t3 - t2;
-        printf("x: %f,y: %f, cx: %f, cy: %f", bpos.x, bpos.y, cpos.x, cpos.y);
+        printf("bx: %f,by: %f, cx: %f, cy: %f", bpos.x, bpos.y, cpos.x, cpos.y);
         std::cout<<", capture: "<<capture<<"ms";
+        std::cout<<", track: "<<track<<"ms";
         std::cout<<", calc: "<<calc<<"ms\n";
     }
 
